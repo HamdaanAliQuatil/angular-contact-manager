4fa69dfec2c1ccf99ee9c945736675497e6791ab
HamdaanAliQuatil <96776914+HamdaanAliQuatil@users.noreply.github.com>
2022-07-29 23:43:13 +0530

    feat: add avatar icon beside contact name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 
 const routes: Routes = [
@@ -22,6 +23,7 @@ const routes: Routes = [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     RouterModule.forRoot(routes),
   ],
   providers: [],
