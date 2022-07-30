cc6e9cf5c969ccbff716edbbdd62d9a88f6664bd
HamdaanAliQuatil <96776914+HamdaanAliQuatil@users.noreply.github.com>
2022-07-30 23:40:23 +0530

    feat: add routes to contact page

--- src/app/contactmanager/components/main-content/main-content.component.ts
+++ src/app/contactmanager/components/main-content/main-content.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-main-content',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-content.component.scss']
 })
 export class MainContentComponent implements OnInit {
+  user: User | undefined;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: UserService
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      this.user = this.service.userById(id);
+    }
+    );
   }
 
 }
